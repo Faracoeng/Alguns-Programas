5e805c8c8028ba4cebbdfb9da08eb79f5b692f27
Faracoeng <alfmazucheli@gmail.com>
2019-10-17 08:03:55 -0300

    Modificacoes de polimorfismo

--- aulas/abstrata/01/src/main/java/poo/Carro.java
+++ aulas/abstrata/01/src/main/java/poo/Carro.java
@@ -4,6 +4,7 @@ public interface Carro {
     public static final String nome = "Carro";
 
     void frear(int intensidade); // Ja é public abstract
+    void acelerar(int intensidade); // Ja é public abstract
 
     default void desligar(){
         System.out.println("Desligando carro");
--- aulas/abstrata/01/src/main/java/poo/Principal.java
+++ aulas/abstrata/01/src/main/java/poo/Principal.java
@@ -4,8 +4,17 @@ public class Principal {
     public static void main(String[] args) {
         Arqueiro arq = new Arqueiro(1403,"Archer",2);
         Cavaleiro cavaleiro = new Cavaleiro(1502,"HorsePower");
-
         arq.mover();
         cavaleiro.mover();
+
+        Personagem vetP[] = new Personagem[4];
+        vetP[0] = new Arqueiro(1456,"Arq",45);
+        vetP[1] = new Cavaleiro(1234,"cava");
+        vetP[2] = new Cavaleiro(1578,"cava02");
+        vetP[3] = new Arqueiro(1456,"Arq02",45);
+        for (Personagem e : vetP){
+            System.out.println(e.getNome()+": ");
+            e.mover();
+        }
     }
 }
