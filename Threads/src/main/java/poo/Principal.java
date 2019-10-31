d1dc0dc9dbeab5913f6a574992c65b59ee843ed2
Faracoeng <alfmazucheli@gmail.com>
2019-10-31 08:22:39 -0300

    Classe contador iniciada - usando um unico contador

--- Threads/src/main/java/poo/Principal.java
+++ Threads/src/main/java/poo/Principal.java
@@ -3,9 +3,9 @@ package poo;
 public class Principal {
     public static void main(String[] args) {
         Contador count = new Contador();
-        Contador count02 = new Contador();
+        //Contador count02 = new Contador();
         Thread comHeranca = new Fluxo1(count);
-        Thread comInterface = new Thread(new Fluxo02(count02));
+        Thread comInterface = new Thread(new Fluxo02(count));
         //Thread outraComHeranca = new Fluxo03("terceiraThread");
 
 
@@ -15,13 +15,13 @@ public class Principal {
         //outraComHeranca.start();
         try {
             comHeranca.join();
-            comInterface.join();
+           // comInterface.join();
         } catch (InterruptedException e) {
             System.err.println(e.toString());
         }
 
         System.out.println(count.getValorAtual());
-        System.out.println(count02.getValorAtual());
+        //System.out.println(count02.getValorAtual());
 
         System.out.println("\nFim do programa");
     }
